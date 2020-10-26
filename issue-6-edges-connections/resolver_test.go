20166699b7e077dcadd12f4d13f2de4735d94502
Richard Lindhout <richardlindhout96@gmail.com>
2020-10-26 19:33:37 +0100

    Enable test

--- issue-6-edges-connections/resolver_test.go
+++ issue-6-edges-connections/resolver_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/stretchr/testify/assert"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 
 	"github.com/web-ridge/gqlgen-sqlboiler-examples/issue-6-edges-connections/models"
@@ -58,103 +59,103 @@ func TestConnectionsWithoutSort(t *testing.T) {
 		handleErr(t, err)
 	}
 	//
-	//userConnection, err := resolver.Query().Users(ctx, fm.ConnectionPagination{
-	//	Forward: &fm.ConnectionForwardPagination{
-	//		First: 10,
-	//		After: nil,
-	//	},
-	//}, nil, nil)
-	//handleErr(t, err)
-	//assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
-	//assert.Equal(t, "Eve20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
-	//assert.Equal(t, "Eve11", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
-	//assert.Equal(t, true, userConnection.PageInfo.HasNextPage, "nextPage not equal")
-	//assert.Equal(t, false, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
-	//
-	//endCursor := userConnection.PageInfo.EndCursor
-	//
-	//userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
-	//	Forward: &fm.ConnectionForwardPagination{
-	//		First: 10,
-	//		After: endCursor,
-	//	},
-	//}, nil, nil)
-	//handleErr(t, err)
-	//assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
-	//assert.Equal(t, "Eve10", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
-	//assert.Equal(t, "Eve1", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
-	//assert.Equal(t, true, userConnection.PageInfo.HasNextPage, "nextPage not equal")
-	//assert.Equal(t, true, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
-	//
-	//endCursor = userConnection.PageInfo.EndCursor
-	//
-	//userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
-	//	Forward: &fm.ConnectionForwardPagination{
-	//		First: 10,
-	//		After: endCursor,
-	//	},
-	//}, nil, nil)
-	//handleErr(t, err)
-	//assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
-	//assert.Equal(t, "Adam20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
-	//assert.Equal(t, "Adam11", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
-	//assert.Equal(t, true, userConnection.PageInfo.HasNextPage, "nextPage not equal")
-	//assert.Equal(t, true, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
-	//
-	//userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
-	//	Forward: &fm.ConnectionForwardPagination{
-	//		First: 100,
-	//		After: nil,
-	//	},
-	//}, nil, nil)
-	//handleErr(t, err)
-	//assert.Equal(t, 40, len(userConnection.Edges), "edges not equal")
-	//assert.Equal(t, "Eve20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
-	//assert.Equal(t, "Adam1", firstNameFromUser(userConnection.Edges[39]), "edges not equal")
-	//assert.Equal(t, false, userConnection.PageInfo.HasNextPage, "nextPage not equal")
-	//assert.Equal(t, false, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
-	//
-	//// BACKWARD PAGINATION
-	//userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
-	//	Backward: &fm.ConnectionBackwardPagination{
-	//		Last:   10,
-	//		Before: nil,
-	//	},
-	//}, nil, nil)
-	//handleErr(t, err)
-	//assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
-	//assert.Equal(t, "Adam10", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
-	//assert.Equal(t, "Adam1", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
-	//assert.Equal(t, true, userConnection.PageInfo.HasPreviousPage, "nextPage not equal")
-	//assert.Equal(t, false, userConnection.PageInfo.HasNextPage, "previousPage not equal")
-	//
-	//startCursor := userConnection.PageInfo.StartCursor
-	//
-	//userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
-	//	Backward: &fm.ConnectionBackwardPagination{
-	//		Last:   10,
-	//		Before: startCursor,
-	//	},
-	//}, nil, nil)
-	//handleErr(t, err)
-	//assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
-	//assert.Equal(t, "Adam20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
-	//assert.Equal(t, "Adam11", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
-	//assert.Equal(t, true, userConnection.PageInfo.HasPreviousPage, "nextPage not equal")
-	//assert.Equal(t, true, userConnection.PageInfo.HasNextPage, "previousPage not equal")
-	//
-	//userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
-	//	Backward: &fm.ConnectionBackwardPagination{
-	//		Last:   100,
-	//		Before: nil,
-	//	},
-	//}, nil, nil)
-	//handleErr(t, err)
-	//assert.Equal(t, "Eve20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
-	//assert.Equal(t, "Adam1", firstNameFromUser(userConnection.Edges[39]), "edges not equal")
-	//assert.Equal(t, 40, len(userConnection.Edges), "edges not equal")
-	//assert.Equal(t, false, userConnection.PageInfo.HasNextPage, "nextPage not equal")
-	//assert.Equal(t, false, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
+	userConnection, err := resolver.Query().Users(ctx, fm.ConnectionPagination{
+		Forward: &fm.ConnectionForwardPagination{
+			First: 10,
+			After: nil,
+		},
+	}, nil, nil)
+	handleErr(t, err)
+	assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
+	assert.Equal(t, "Eve20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
+	assert.Equal(t, "Eve11", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
+	assert.Equal(t, true, userConnection.PageInfo.HasNextPage, "nextPage not equal")
+	assert.Equal(t, false, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
+
+	endCursor := userConnection.PageInfo.EndCursor
+
+	userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
+		Forward: &fm.ConnectionForwardPagination{
+			First: 10,
+			After: endCursor,
+		},
+	}, nil, nil)
+	handleErr(t, err)
+	assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
+	assert.Equal(t, "Eve10", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
+	assert.Equal(t, "Eve1", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
+	assert.Equal(t, true, userConnection.PageInfo.HasNextPage, "nextPage not equal")
+	assert.Equal(t, true, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
+
+	endCursor = userConnection.PageInfo.EndCursor
+
+	userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
+		Forward: &fm.ConnectionForwardPagination{
+			First: 10,
+			After: endCursor,
+		},
+	}, nil, nil)
+	handleErr(t, err)
+	assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
+	assert.Equal(t, "Adam20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
+	assert.Equal(t, "Adam11", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
+	assert.Equal(t, true, userConnection.PageInfo.HasNextPage, "nextPage not equal")
+	assert.Equal(t, true, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
+
+	userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
+		Forward: &fm.ConnectionForwardPagination{
+			First: 100,
+			After: nil,
+		},
+	}, nil, nil)
+	handleErr(t, err)
+	assert.Equal(t, 40, len(userConnection.Edges), "edges not equal")
+	assert.Equal(t, "Eve20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
+	assert.Equal(t, "Adam1", firstNameFromUser(userConnection.Edges[39]), "edges not equal")
+	assert.Equal(t, false, userConnection.PageInfo.HasNextPage, "nextPage not equal")
+	assert.Equal(t, false, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
+
+	// BACKWARD PAGINATION
+	userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
+		Backward: &fm.ConnectionBackwardPagination{
+			Last:   10,
+			Before: nil,
+		},
+	}, nil, nil)
+	handleErr(t, err)
+	assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
+	assert.Equal(t, "Adam10", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
+	assert.Equal(t, "Adam1", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
+	assert.Equal(t, true, userConnection.PageInfo.HasPreviousPage, "nextPage not equal")
+	assert.Equal(t, false, userConnection.PageInfo.HasNextPage, "previousPage not equal")
+
+	startCursor := userConnection.PageInfo.StartCursor
+
+	userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
+		Backward: &fm.ConnectionBackwardPagination{
+			Last:   10,
+			Before: startCursor,
+		},
+	}, nil, nil)
+	handleErr(t, err)
+	assert.Equal(t, 10, len(userConnection.Edges), "edges not equal")
+	assert.Equal(t, "Adam20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
+	assert.Equal(t, "Adam11", firstNameFromUser(userConnection.Edges[9]), "edges not equal")
+	assert.Equal(t, true, userConnection.PageInfo.HasPreviousPage, "nextPage not equal")
+	assert.Equal(t, true, userConnection.PageInfo.HasNextPage, "previousPage not equal")
+
+	userConnection, err = resolver.Query().Users(ctx, fm.ConnectionPagination{
+		Backward: &fm.ConnectionBackwardPagination{
+			Last:   100,
+			Before: nil,
+		},
+	}, nil, nil)
+	handleErr(t, err)
+	assert.Equal(t, "Eve20", firstNameFromUser(userConnection.Edges[0]), "edges not equal")
+	assert.Equal(t, "Adam1", firstNameFromUser(userConnection.Edges[39]), "edges not equal")
+	assert.Equal(t, 40, len(userConnection.Edges), "edges not equal")
+	assert.Equal(t, false, userConnection.PageInfo.HasNextPage, "nextPage not equal")
+	assert.Equal(t, false, userConnection.PageInfo.HasPreviousPage, "previousPage not equal")
 
 	// Adam
 	for i := 0; i < 20; i++ {
